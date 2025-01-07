2ed3138c00a8afafb409cc287838a0b4ea63188c
Neil Chowdhary <ashuchow@gmail.com>
2025-01-07 19:55:42 +0530

    moved superuser password to env file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,18 +4,26 @@ const bcrypt = require('bcrypt'); // CommonJS require
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash('superuserpassword123', 10);
+  const superUserPassword = process.env.SUPER_USER_PASSWORD;
+
+  if (!superUserPassword) {
+    throw new Error('SUPER_USER_PASSWORD is not defined in the environment variables.');
+  }
+
+  const hashedPassword = await bcrypt.hash(superUserPassword, 10);
 
   // Create the super user
-  const superUser = await prisma.user.create({
-    data: {
+  const superUser = await prisma.user.upsert({
+    where: { username: 'superuser' },
+    update: {},
+    create: {
       username: 'superuser',
       password: hashedPassword,
       role: 'SUPER_USER',
     },
   });
 
-  console.log('Super user created:', superUser);
+  console.log('Super user created or already exists:', superUser);
 }
 
 main()
